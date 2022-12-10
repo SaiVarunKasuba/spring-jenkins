9765fd3b0aa696635252e850e664fd8f10de4a9e
SaiVarunKasuba <kasubasavarun@gmail.com>
2022-12-10 22:08:50 +0530

    Initial commit

---
+++ src/main/java/com/sai/test/TestClass.java
@@ -0,0 +1,8 @@
+package com.sai.test;
+
+public class TestClass {
+public int add(int a, int b)
+{
+	return a+b;
+}
+}
---
+++ src/test/java/com/sai/test/TestClassTest.java
@@ -0,0 +1,14 @@
+package com.sai.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestClassTest {
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
